21e837b4d8434261d2700f2ce391a46da836df53
dasosann <goodtony0403@gmail.com>
2024-10-25 03:07:55 +0900

    change to css all

--- app/src/page/LoginHeader.jsx
+++ app/src/page/LoginHeader.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import '../style/LoginHeader.css';
 const LoginHeader = () => {
     return (
-        <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"30px 40px 10px 40px"}}>
-            <div style={{display:"flex",gap:"3vw"}}>
+        <div className='header-style'>
+            <div className='header-gap'>
                 <button className='header-button'>Home</button>
                 <button className='header-button'>MENU</button>
             </div>
-            <div style={{display:"flex",gap:"3vw"}}>
+            <div className='header-gap'>
                 <button className='header-button'>MY PAGE</button>
             </div>
         </div>
--- app/src/page/MainHeader.jsx
+++ app/src/page/MainHeader.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import "../style/MainHeader.css"
 const MainHeader = () => {
     return (
-        <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"30px 40px"}}>
-            <div style={{display:"flex",gap:"3vw"}}>
+        <div className='header-style'>
+            <div className='header-gap'>
                 <button className='header-button'>Home</button>
                 <button className='header-button'>MENU</button>
             </div>
-            <div style={{display:"flex",gap:"3vw"}}>
+            <div className='header-gap'>
                 <button className='header-button'>LOGIN</button>
                 <button className='header-button'>MY PAGE</button>
             </div>
